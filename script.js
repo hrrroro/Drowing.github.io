3faec74e3142fe8e369819bac13ec7dc3d2733e1
hrrroro <vldzm126@gmail.com>
2025-04-30 14:23:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 function toggleSubMenu() {
   const subMenu = document.getElementById('subMenu');
-  if (subMenu.style.display === 'block') {
-    subMenu.style.display = 'none';
-  } else {
-    subMenu.style.display = 'block';
-  }
+  subMenu.classList.toggle('show'); // 'show' 클래스를 토글하여 부드럽게 나타나게 합니다.
 }
-
