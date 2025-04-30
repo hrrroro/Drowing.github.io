0d8865608a7475b5fb70ea0b9c23a8f4802b80bc
hrrroro <vldzm126@gmail.com>
2025-04-30 14:14:30 +0900

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleSubMenu() {
+  const subMenu = document.getElementById('subMenu');
+  if (subMenu.style.display === 'block') {
+    subMenu.style.display = 'none';
+  } else {
+    subMenu.style.display = 'block';
+  }
+}
+
