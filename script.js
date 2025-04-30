400ad35597d7a6b513cca7d9194367cb2fced758
hrrroro <vldzm126@gmail.com>
2025-04-30 14:44:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleSubMenu() {
-  const subMenu = document.getElementById('subMenu');
-  subMenu.classList.toggle('show'); // 'show' 클래스를 토글하여 하위 메뉴를 보이게/숨기게 함
+  const subMenu = document.getElementById("subMenu");
+  subMenu.classList.toggle("show");
 }
